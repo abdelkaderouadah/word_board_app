b8df8515835d6b40efed4aaf3ca0b9b5124d7626
abdelkaderouadah <ouadah.abdelkader9@gmail.com>
2021-12-14 14:50:45 +0100

    Changing colors

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Word Board',
       theme: ThemeData(
-        primaryColor: Colors.amber[700],
+        primaryColor: Colors.white,
       ),
       home: const HomeScreen(),
     );
